fba624238bf6f67aa5b80843b0207ddf9ee0a718
Adam Woodworth <adamwoodworth94@siu.edu>
2015-04-17 02:15:36 -0500

    fix fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( batman.ttf )
-Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
+# Rails.application.config.assets.precompile += ['fonts/batman.ttf']
