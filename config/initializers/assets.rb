f7612698b518aca998896fda6c744d475f84a36c
Adam Woodworth <adamwoodworth94@siu.edu>
2015-04-17 01:41:18 -0500

    update assets update precompile
    fix fonts
    
    update produciton
    
    remove batman font from style
    
    add font back
    
    update assets
    
    remove batman again :(

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( batman.ttf )
+# Rails.application.config.assets.precompile += ['fonts/batman.ttf']
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf)
