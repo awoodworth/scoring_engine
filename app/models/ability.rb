c9266be7a0d2d31aea39e429a8efaba4435259e3
Adam Woodworth <adamwoodworth94@siu.edu>
2016-11-19 23:22:41 -0600

    fix turbo links 5 file download issue

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,27 +8,21 @@ class Ability
     # since some users will be on white team and an admin, etc
     # we put the most access at the bottom
 
-    # everyone has to be able to read the event to be able to access that model
-    can :read, Event do |event|
-      event.available?
-    end
-
     if user.in_group?(:blue_team)
       can :read, Inject do |inject|
         inject.available?
       end
       can :manage, [InjectResponse, FlagSubmission], user_id: user.id
-      can :read, Guide
+      can :read, [Guide, Event]
     end
 
     if user.in_group?(:white_team)
-      can :manage, [Inject, Guide]
-      can :manage, InjectResponse
+      can :manage, [Inject, InjectResponse, Guide]
       # cannot :destroy, InjectResponse
     end
 
     if user.in_group?(:red_team)
-      # nothing
+      can :read, Event
     end
 
     if user.in_group?(:admin)
@@ -37,5 +31,6 @@ class Ability
         event.available? || event.past?
       end
     end
+
   end
 end
